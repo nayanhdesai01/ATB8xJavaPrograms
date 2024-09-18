c7baf1af5afe1c8a313db5ba35ffe1b8d902a8f9
Nayan Patil <nayanhdesai@gmail.com>
2024-09-19 00:09:19 +0530

    Practice program 1

---
+++ src/Sept/PraticePrograms/PP001_Table.java
@@ -0,0 +1,14 @@
+//Print  lines of output; each line  (where ) contains the  of  in the form:
+//N x i = result.
+
+package Sept.PraticePrograms;
+public class PP001_Table {
+    public static void main(String[] args){
+int num= 2;
+
+        System.out.println("Table of"+num);
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d\tx\t%d\t=\t%d\n",num,i,num*i);
+        }
+    }
+}
