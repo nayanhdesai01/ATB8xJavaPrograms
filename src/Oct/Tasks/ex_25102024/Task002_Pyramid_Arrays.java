d085219bbb03b2ab38865a56cee9619cb4b4a0ec
Nayan Patil <nayanhdesai@gmail.com>
2024-11-09 01:45:03 +0530

    Task for 25th Oct is complete

--- src/Oct/Tasks/ex_25102024/Task002_Pyramid_Arrays.java
+++ src/Oct/Tasks/ex_25102024/Task002_Pyramid_Arrays.java
@@ -10,10 +10,19 @@ public class Task002_Pyramid_Arrays {
         int n = sc.nextInt();
         int[][] stars = new int[n][n];
         for (int i = 0; i<n; i+=2) {
-            for (int j = 0; j<=i; j++) {
+        for (int j = 0; j<=i; j++) {
                 System.out.print("*");
-            }
+          }
             System.out.println();
         }
+//        for (int i=0; i<n; i+=2){  //Outer loop for rows
+//            for (int j=n-i; j>1; j--)  {       //inner loop for space
+//                System.out.print(" ");       //prints space between two stars
+//            }
+//            for (int k=0; k<=i; k++ ) {    //inner loop for columns
+//                System.out.print("* ");    //prints star
+//            }
+//            System.out.println();       //throws the cursor in a new line after printing each line
+//        }
     }
 }
